a0160354c76f1f7d05a2cc4a658c95651f3b1e4e
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-04-07 17:41:11 +0200

    Sistemato con classlist add

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
 let hamburgerIcon = document.querySelector(".fa-bars");
 let hiddenMenu = document.querySelector(".hamburger-menu");
-let xIcon = document.querySelector(".fa-times")
+let xIcon = document.querySelector(".fa-times");
 
 hamburgerIcon.addEventListener("click", function(){
-  hiddenMenu.classList.add = ".hamburger-menu.active";
+  hiddenMenu.classList.add('active');
 });
 
 xIcon.addEventListener("click", function(){
-  hiddenMenu.classList.cssText = ".hamburger-menu.";
+  hiddenMenu.classList.remove('active');
 });
